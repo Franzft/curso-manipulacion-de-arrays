ac0b2b633ff818cc80dcd7668f8943f57e70811c
Franz Toledo <franz.rigbon.04@gmail.com>
2022-04-21 18:11:38 -0500

    Clase flat

--- server/12-concat.js
+++ server/12-concat.js
@@ -9,10 +9,10 @@ for (let index = 0; index < othersElements.length; index++) {
 
 const rta = elements.concat(othersElements);
 const rta2 = [...elements, ...othersElements];
-const rta3 = [...elements, ...'random'];
-console.log('for', newArray);
-console.log('concat', rta);
-console.log('...', rta2);
-console.log('rta3', rta3);
+const rta3 = [...elements, ..."random"];
+console.log("for", newArray);
+console.log("concat", rta);
+console.log("...", rta2);
+console.log("rta3", rta3);
 elements.push(...othersElements);
-console.log('elements', elements);
+console.log("elements", elements);
