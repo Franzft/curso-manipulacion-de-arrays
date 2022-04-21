350db0509fb8a9bb36f4ea2302752e706d8516cd
Franz Toledo <franz.rigbon.04@gmail.com>
2022-04-21 17:01:48 -0500

    clase reduce-reloaded

---
+++ server/06-reduce-reloaded.js
@@ -0,0 +1,51 @@
+const items = [1,1,3,4,4];
+
+const rta = items.reduce ((obj, item) => {
+    if (!obj[item]) {
+        obj[item] = 1;
+    } else {
+        obj[item] = obj[item] + 1;
+    }
+    return obj;
+}, {});
+console.log (rta)
+
+const data = [
+    {
+      name: "Franz",
+      level: "low",
+    },
+    {
+      name: "Andrea",
+      level: "medium",
+    },
+    {
+      name: "Zulema",
+      level: "hight",
+    },
+    {
+      name: "Santiago",
+      level: "low",
+    },
+    {
+      name: "Valentina",
+      level: "medium",
+    },
+    {
+      name: "Lucia",
+      level: "hight",
+    },
+  ];
+
+  const rta1 = data
+.map(item => item.level)
+.reduce((obj, item) => {
+    if (!obj[item]) {
+        obj[item] = 1;
+    } else {
+        obj[item] = obj[item] + 1;
+    }
+    return obj;
+}, {});
+
+console.log(rta1);
