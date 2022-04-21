31f1451977d4be2dd05a64b7fde3ef4e43f9b0df
Franz Toledo <franz.rigbon.04@gmail.com>
2022-04-21 17:03:06 -0500

    clase reduce-reloaded reto

--- server/06-reduce-reloaded.js
+++ server/06-reduce-reloaded.js
@@ -49,3 +49,21 @@ const data = [
 }, {});
 
 console.log(rta1);
+//reto 
+const number = [3, 10, 9, 4, 3, 1, 8, 4, 7, 6];
+const result = number.reduce((obj, item) => {
+  if (item <= 5) {
+    obj['1-5']++
+  } else if (item <= 8) {
+    obj['6-8']++
+  } else {
+    obj['9-10']++
+  }
+  return obj
+}, {
+  '1-5': 0,
+  '6-8': 0,
+  '9-10': 0
+})
+
+console.log(result)
